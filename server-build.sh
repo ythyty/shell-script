f476ca877ec962b216b0215b84c2aab065d167bc
zhangyl2 <zhangyl2@ziroom.com>
2018-12-17 11:16:37 +0800

    1

---
+++ server-build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+WORKSPACE=$1
+BUILD_NUMBER=$2
+export PROJECT=$3
+export SPRING_PROFILES_ACTIVE=$4
+export JAVA_MEM_OPTS=$5
+
+DATE=$(date +%F)
+BASE_DIR=/rwda/runtime
+HOME_DIR=${BASE_DIR}/${PROJECT}
+BIN_DIR=${HOME_DIR}/bin
+BACKUP_DIR=${HOME_DIR}/backup
+export LOG_DIR=${HOME_DIR}/logs
+
+if [ ! -d ${HOME_DIR} ];then
+    mkdir -p ${HOME_DIR}
+fi
+
+if [ ! -d ${BIN_DIR} ];then
+    mkdir -p ${BIN_DIR}
+fi
+
+if [ ! -d ${BACKUP_DIR} ];then
+    mkdir -p ${BACKUP_DIR}
+fi
+
+if [ ! -d ${LOG_DIR} ];then
+    mkdir -p ${LOG_DIR}
+fi
+
+export JARFILE=`find ${BIN_DIR}/ -name "*.jar"`
+echo "JARFILE：${JARFILE}"
+
+if [ -f ${JARFILE} ];then
+    JARNAME=${JARFILE##*/}
+    cp -f ${JARFILE} ${BACKUP_DIR}/${JARNAME%.*}-${DATE}-${BUILD_NUMBER}.jar
+fi
+
+rsync -var --delete ${WORKSPACE}/${PROJECT}/target/ ${BIN_DIR}/
+
+echo "脚本目录：$(dirname $0)"
+cd $(dirname $0)
+sh spring-boot.sh restart
+
+sleep 5
+echo "获取日志"
+cat ${LOG_DIR}/stdout.log
+
+echo "查看端口号："
+ps -ef | grep java | grep ${PROJECT} | grep -v grep | awk '{print $2}'
+PID=`ps -ef | grep java | grep ${PROJECT} | grep -v grep | awk '{print $2}'`
+if [ -z "${PID}" ];then
+    echo "${PROJECT} 启动失败"
+else
+    echo "${PROJECT} 启动完成"
+fi
+
+cd ${BACKUP_DIR}
+ls -lt|awk 'NR>5{print $NF}'|xargs rm -rf
